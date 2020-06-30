390f8bf91910765ddd6506a5294548589f0c0db2
Iago Balbino <iago_nobre@hotmail.com>
2020-06-29 21:57:51 -0300

    Criado banco de dados com MongoDB

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
+// Iniciando o app
 const app = express();
 
+// Iniciando o DB
+mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParse: true });
+
+// Primeira rota
 app.get('/', (req, res) => {
     res.send('Não esquece de subir pro Github');
 });
